35efde083b15d292047bb7aa14bf65e34192893f
hayeoung <lante1117@naver.com>
2015-12-14 16:42:03 +0900

    sdl2적용
    ㅇ

--- 2DGP/mygame.py
+++ 2DGP/mygame.py
@@ -2,4 +2,12 @@ import game_framework
 
 import start_state
 
+import platform
+import os
+
+if platform.architecture()[0] == '32bit':
+    os.environ["PYSDL2_DLL_PATH"] = "./SDL2/x86/"
+elif platform.architecture()[0] == '64bit':
+    os.environ["PYSDL2_DLL_PATH"] = "./SDL2/x64"
+
 game_framework.run(start_state)
